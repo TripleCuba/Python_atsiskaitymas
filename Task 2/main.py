af0d7f21dfa84c293cb92180fee45cbfea4c525f
TripleCuba <justasbrazaitis@gmail.com>
2022-06-16 19:41:01 +0300

    task 1&2 done

--- Task 2/main.py
+++ Task 2/main.py
@@ -8,14 +8,37 @@
 
 # Pastaba: rezultatai turi būti išrikiuoti abėcėlės tvarka
 
+
 users = [
-  { "id": '1', "name": 'John Smith', "age": 20 },
-  { "id": '2', "name": 'Ann Smith', "age": 24 },
-  { "id": '3', "name": 'Tom Jones', "age": 31 },
-  { "id": '4', "name": 'Rose Peterson', "age": 17 },
-  { "id": '5', "name": 'Alex John', "age": 25 },
-  { "id": '6', "name": 'Ronald Jones', "age": 63 },
-  { "id": '7', "name": 'Elton Smith', "age": 16 },
-  { "id": '8', "name": 'Simon Peterson', "age": 30 },
-  { "id": '9', "name": 'Daniel Cane', "age": 51 },
+    {"id": '1', "name": 'John Smith', "age": 20},
+    {"id": '2', "name": 'Ann Smith', "age": 24},
+    {"id": '3', "name": 'Tom Jones', "age": 31},
+    {"id": '4', "name": 'Rose Peterson', "age": 17},
+    {"id": '5', "name": 'Alex John', "age": 25},
+    {"id": '6', "name": 'Ronald Jones', "age": 63},
+    {"id": '7', "name": 'Elton Smith', "age": 16},
+    {"id": '8', "name": 'Simon Peterson', "age": 30},
+    {"id": '9', "name": 'Daniel Cane', "age": 51},
 ]
+
+
+def get_user_average_age(dict):
+    total_age = 0
+    for age in dict:
+        total_age += age['age']
+
+    average_age = total_age / len(dict)
+    print(average_age)
+    return average_age
+
+
+def get_user_names(dict):
+    new_list = []
+    for name in dict:
+        new_list.append(name['name'])
+    new_list.sort()
+    print(new_list)
+
+
+get_user_average_age(users)
+get_user_names(users)
